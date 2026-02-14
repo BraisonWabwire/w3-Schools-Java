a6767637b9866dd3aba9e6146a9ca21b77739c8c
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-14 11:26:55 +0300

    Wide Casting in JAVA

--- Main.java
+++ Main.java
@@ -1,21 +1,9 @@
-public class Main{
+// Wide Casting (Automatic)
+public class Main {
     public static void main(String[] args) {
-       String studentID = "CCS/00015/021";
-       String firstName = "Braison";
-       String lastName = null;
-       int studentAge = 22;
-       float studentFee = 150000.00f;
-       float studentGrade = 70.5f;
-       char var1= 90;
-       var x = 10;
-
-        System.out.println("Student ID: " + studentID);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Student Age: " + studentAge);
-        System.out.println("Student Fee: " + studentFee);
-        System.out.println("Student Grade: " + studentGrade);
-        System.out.println("Variable 1: " + var1);
-        System.out.println("Variable x: " + x);
-    }
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+    System.out.println("myInt: " + myInt); // Output: 9
+    System.out.println("myDouble: " + myDouble); // Output: 9.0
+}
 }
