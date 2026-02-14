95781777c97d0848c1835f66629feef23c0b0bef
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-14 11:28:31 +0300

    Narrow Casting

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-// Wide Casting (Automatic)
+// Narrowing Casting (Manual)
 public class Main {
     public static void main(String[] args) {
-    int myInt = 9;
-    double myDouble = myInt; // Automatic casting: int to double
-    System.out.println("myInt: " + myInt); // Output: 9
-    System.out.println("myDouble: " + myDouble); // Output: 9.0
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
 }
 }
