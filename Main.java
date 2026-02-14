8bd3535fa81dc05cd8b0bd53af9e5939243aa3f9
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-14 10:43:31 +0300

    Real world Example

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 public class Main{
     public static void main(String[] args) {
-        String Lname = "Wabwire";
-       String Fname="Braison";
-       String FullName= Fname+ " "+ Lname;
-        System.out.println("Hello "+FullName+",Welcome to Java Programming!");
+       String studentID = "CCS/00015/021";
+       String firstName = "Braison";
+       String lastName = "Wabwire";
+       int studentAge = 22;
+       float studentFee = 150000.00f;
+       float studentGrade = 70.5f;
+
+        System.out.println("Student ID: " + studentID);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
     }
 }
