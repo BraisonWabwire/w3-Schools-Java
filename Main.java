dcd25699314c790ea7827fa7e0da5fc762602924
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-14 11:55:05 +0300

    Understanding JAVA strings

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
-// Narrowing Casting (Manual)
-public class Main {
-    public static void main(String[] args) {
-        double myDouble = 9.78d;
-        int myInt = (int) myDouble;
-        System.out.println(myDouble);   // Outputs 9.78
-        System.out.println(myInt);      // Outputs 9
-}
+// String methods
+// 1. To upper case
+// 2. To lower case
+// 3. Length of the string
+// 4. Finding a character in the string
+
+public class Main{
+    public static void main(String[] args){
+        String str = "Hello Braison welcome to Java Programming";
+        System.out.println("Original String: "+ str);
+
+        // To upper case
+        String upperStr= str.toUpperCase();
+        System.out.println("Upper Case: "+ upperStr);
+
+        // To lower case
+        String lowerStr= str.toLowerCase();
+        System.out.println("Lower Case: "+ lowerStr);
+
+        // Length of the string
+        int length = str.length();
+        System.out.println("Length of the string: "+ length);
+
+        // Finding a character in the string using indexOf
+        System.out.println("Index of 'Braison': "+ str.indexOf("Braison"));
+
+        // Using charAt to find a character at a specific index
+        System.out.println("Character at index 6: "+ str.charAt(6));
+    }
 }
