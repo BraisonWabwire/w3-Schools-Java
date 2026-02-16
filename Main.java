49d46bb953e1696e038a3dcd6f69b1fa7b5ad02f
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-16 08:20:23 +0300

    Java for loop

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
-// Real world example
+// Java For Loop
 public class Main {
-
-     public static void main(String[] args){
-        int dice = 1;
-        while( dice <=6){
-            if(dice == 6){
-                System.out.println("You win!");
-            } else {
-                System.out.println("You lose!");
-            }
-            dice = dice + 1;
+    public static void main(String[] args) {
+        for (int i =100; i > 0; i--){
+            System.out.println(i);
         }
-     }
+    }
 }
