de9848734c6252ab752de2ce676a904abc7ecd18
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-16 08:23:21 +0300

    Java nested loops

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-// Java For Loop
+// Java nested loops
 public class Main {
     public static void main(String[] args) {
-        for (int i =100; i > 0; i--){
-            System.out.println(i);
+        for (int i=1; i <=3; i++){
+            System.out.println("Outer loop: " + i);
+            for (int j=1; j<=3; j++){
+                System.out.println("  Inner loop: " + j);
+            }
         }
     }
 }
