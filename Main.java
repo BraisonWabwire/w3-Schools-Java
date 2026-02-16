942be54980abce02c1b940a67bf061117bf05207
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-16 07:05:25 +0300

    If else JAVA

--- Main.java
+++ Main.java
@@ -1,30 +1,16 @@
-// String methods
-// 1. To upper case
-// 2. To lower case
-// 3. Length of the string
-// 4. Finding a character in the string
-
-public class Main{
+// The else if condition in Java
+public class Main {
     public static void main(String[] args){
-        String str = "Hello Braison welcome to Java Programming";
-        System.out.println("Original String: "+ str);
-
-        // To upper case
-        String upperStr= str.toUpperCase();
-        System.out.println("Upper Case: "+ upperStr);
-
-        // To lower case
-        String lowerStr= str.toLowerCase();
-        System.out.println("Lower Case: "+ lowerStr);
-
-        // Length of the string
-        int length = str.length();
-        System.out.println("Length of the string: "+ length);
-
-        // Finding a character in the string using indexOf
-        System.out.println("Index of 'Braison': "+ str.indexOf("Braison"));
-
-        // Using charAt to find a character at a specific index
-        System.out.println("Character at index 6: "+ str.charAt(6));
+        int weather = 4;
+        if (weather == 1){
+            System.out.println("Bring an umbrella.");
+    
+        }
+        else if (weather == 2){
+            System.out.println("Wear sun glasses.");
+        }
+        else{
+            System.out.println("Go outside and enjoy the weather.");
+        }
     }
 }
