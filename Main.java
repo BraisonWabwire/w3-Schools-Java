150f7fa8ddae9e14e2e9790449a6c0cc333fb061
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-17 09:53:50 +0300

    Java methods

--- Main.java
+++ Main.java
@@ -1,21 +1,13 @@
-// Java multidimentional arrays
+// Java Methods 
 public class Main {
-    public static void main(String[] args) {
-       int [][] numbers = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
-       };
 
-        System.out.println(numbers[0][0]);
-        System.out.println(numbers[0][1]);
+     public static void myMethod(){
+        System.out.println("I just got executed!");
+     }
 
-
-        // Looping through a multidimentional array
-        for (int row=0; row < numbers.length; row++){
-            for (int col=0; col < numbers[row].length; col++){
-                 System.out.println("myNumbers[" + row + "][" + col + "] = " + numbers[row][col]);
-            }
-        }
-    }
+     public static void main (String[] args){
+        myMethod();
+        myMethod();
+        myMethod();
+     }
 }
