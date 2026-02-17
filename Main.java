14da9d3ccbcab20534e5043cd8a24ae69dfe0e99
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-17 21:29:24 +0300

    Constructor methods

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
-public class Main{
-      public void fullThrottle(){
-          System.out.println("The car is going as fast as it can!");
-      }
-      public void speed(int maxSpeed){
-          System.out.println("Max speed is: " + maxSpeed);
-      }
+// Constructor methods in java
+public class Main {
+
+     String name;
+     String email;
+     int age;
+
+
+     // Constructor method
+     public Main(String name, String email, int age){
+          this.name = name;
+          this.email = email;
+          this.age = age;
+     }
+
      public static void main(String[] args){
-         Main obj = new Main();
-          obj.fullThrottle();
-          obj.speed(200);
-     
-}
+
+          Main obj = new Main("Braison", "braison@example.com", 25);
+          System.out.println(obj.name);
+          System.out.println(obj.email);
+          System.out.println(obj.age);
+
+     }
 }
