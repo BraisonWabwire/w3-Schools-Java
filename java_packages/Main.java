e375d32776e5c29c277c0be1e41b0a4d37e994b4
BraisonWabwire <braisonwabwire2003@gmail.com>
2026-02-18 10:58:52 +0300

    Java inheritance

---
+++ java_packages/Main.java
@@ -0,0 +1,15 @@
+package java_packages;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+
+
+        }
+    
+    }
